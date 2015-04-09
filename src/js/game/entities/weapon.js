2471477e0b7aa5e566e77705e2b084b2c8a214a3
Chris Garson <cgarson@gmail.com>
2015-04-09 08:33:22 -0400

    refactored

--- src/js/game/entities/weapon.js
+++ src/js/game/entities/weapon.js
@@ -5,21 +5,38 @@ module.exports = function() {
   var CLIP_SIZE = 3;
   var RELOAD_SPEED = 60; //speed to reload each bullet in game frames
 
+  var timers = {
+    shot: 0,
+    reload: 0
+  };
+
+  var checkReload = function () {
+    if (logic.ammo < CLIP_SIZE) {
+      setAmmo(logic.ammo + 1);
+      if (logic.ammo < CLIP_SIZE) {
+        timers.reload = RELOAD_SPEED;
+      }
+    }
+  };
+
+  var setAmmo = function (count) {
+    logic.ammo = count;
+    console.log('ammo: '+count);
+  };
+
   var logic = {
-    shotTimer: 0,
-    ammo: CLIP_SIZE,
-    reloadTimer: 0
+    ammo: CLIP_SIZE
   };
 
   logic.shoot = function (game, player, bullets) {
     var baseSpeed = 1000;
 
-    if (logic.shotTimer < game.time.now) {
-      logic.shotTimer = game.time.now + 275;
+    if (timers.shot < game.time.now) {
+      timers.shot = game.time.now + 275;
       if (logic.ammo === 0) {
         logic.reload();
       } else {
-        logic.setAmmo(logic.ammo - 1);
+        setAmmo(logic.ammo - 1);
         var bullet,
             rotation = Utilities.calculateRotation(game, player),
             yModifier = Math.sin(rotation),
@@ -39,27 +56,19 @@ module.exports = function() {
     }
   };
 
-  logic.setAmmo = function (count) {
-    logic.ammo = count;
-    console.log('ammo: '+count);
-  };
-
   logic.reload = function () {
-    if (logic.reloadTimer === 0) {
-      logic.reloadTimer = RELOAD_SPEED;
+    if (timers.reload === 0) {
+      timers.reload = RELOAD_SPEED;
       console.log('reloading');
     }
   };
 
   logic.updateReloadTimer = function () {
-    if (logic.reloadTimer > 0) {
-      logic.reloadTimer--;
+    if (timers.reload > 0) {
+      timers.reload--;
 
-      if (logic.reloadTimer === 0 && logic.ammo < CLIP_SIZE) {
-        logic.setAmmo(logic.ammo + 1);
-        if (logic.ammo < CLIP_SIZE) {
-          logic.reloadTimer = RELOAD_SPEED;
-        }
+      if (timers.reload === 0) {
+        checkReload();
       }
     } 
   };
