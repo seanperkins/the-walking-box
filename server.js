c3d0703a2792a729aed9aee1c84c020b28b60416
Samuel Reed <samuel.trace.reed@gmail.com>
2015-04-11 09:27:15 -0400

    Zip it up

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic('./build')).listen(process.env.PORT);
+var compression = require('compression');
+
+connect()
+.use(compression())
+.use(serveStatic('./build'))
+.listen(process.env.PORT);
